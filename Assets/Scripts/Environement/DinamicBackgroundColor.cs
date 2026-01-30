55a0b370d27286a05a7135563d2b5c20ceee05eb
ltisserand-hub <a.tisserand@nouvellesimages.xyz>
2026-01-30 17:02:52 +0100

    [Minor] WIP color change

--- Assets/Scripts/Environement/DinamicBackgroundColor.cs
+++ Assets/Scripts/Environement/DinamicBackgroundColor.cs
@@ -1,17 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DinamicBackgroundColor : MonoBehaviour
 {
     [SerializeField] float transitionTime = 5f;
     [Header("Colors")]
-    [SerializeField] Color firstColor = Color.white;
-    [SerializeField] Color secondColor = Color.black;
+    [SerializeField] List<Color> color;
     private Color currentColor;
     private float transitionCurrentTime = 0f;
     void Update()
     {
         transitionCurrentTime = Mathf.PingPong(Time.unscaledTime,transitionTime);
-        currentColor = Color.Lerp(firstColor, secondColor, transitionCurrentTime/transitionTime);
+        currentColor = Color.Lerp(color[0], color[1], transitionCurrentTime/transitionTime);
         Camera.main.backgroundColor = currentColor;
       
     }
