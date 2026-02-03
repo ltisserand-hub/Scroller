2e749ee4e5f596d3ab4cf7005feb084c68cc2614
ltisserand-hub <a.tisserand@nouvellesimages.xyz>
2026-02-03 17:29:45 +0100

    [minor] same for all

--- Assets/Scripts/Environement/ColorAnimation.cs
+++ Assets/Scripts/Environement/ColorAnimation.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 public class ColorAnimation : MonoBehaviour
 {
     [SerializeField] float transitionTime = 5f;
-        [SerializeField] Color firstColor = Color.white;
-        [SerializeField] Color secondColor = Color.black;
-        [SerializeField] Color currentColor;
-        private float transitionCurrentTime = 0f;
-        void Update()
-        {
-            transitionCurrentTime = Mathf.PingPong(Time.unscaledTime,transitionTime);
-            currentColor = Color.Lerp(firstColor, secondColor, transitionCurrentTime/transitionTime);
-            GetComponent<SpriteRenderer>().color = currentColor;
-          
-        }
+    [SerializeField] Gradient colors;
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().color = colors.Evaluate(Mathf.PingPong(Time.unscaledTime,transitionTime)/transitionTime);
+    }
 }
